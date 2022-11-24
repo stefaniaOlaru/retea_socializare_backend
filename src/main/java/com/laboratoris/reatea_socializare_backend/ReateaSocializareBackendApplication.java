8aa0474dd6faf64310485677ea822e412f8b2b30
stefaniaOlaru <stefaniaolaru11@gmail.com>
2022-11-24 18:59:06 +0200

    Project strucutre

---
+++ src/main/java/com/laboratoris/reatea_socializare_backend/ReateaSocializareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.laboratoris.reatea_socializare_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReateaSocializareBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReateaSocializareBackendApplication.class, args);
+    }
+
+}
